55c33c070961a27da06a7320d23c639bb42a62f2
bart <bart.hakstege@student.hu.nl>
2020-01-13 10:23:25 +0100

    Updated checksum

--- SIGNING/create_sum.py
+++ SIGNING/create_sum.py
@@ -2,8 +2,8 @@ import os
 import subprocess
 
 def create_sum():
-	os.chdir("/root/")
-	cmd = "sha1sum /root/Scripts/* > /root/Scripts/.sha1"
+	os.chdir("/root/Automation3/")
+	cmd = "sha1sum /root/Automation3/SERVER_CLIENT/* > /root/Automation3/SERVER_CLIENT/.sha1"
 	return_value = subprocess.call(cmd, shell=True) # returned exit code
 	if return_value == 0:
 		print("Succesfully made a SHA1 sum!")
