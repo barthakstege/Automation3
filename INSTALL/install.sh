d0fb52abb3acda4413744bf59d10cea11f9119c4
bart <bart.hakstege@student.hu.nl>
2020-01-13 12:56:57 +0100

    Added XML configuration functionality

--- INSTALL/install.sh
+++ INSTALL/install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 ## Gemaakt door Bart :-)
 echo "AUTOMATION INSTALL SCRIPT V1.0"
+CLIENT_IP=$(grep -oPm1 "(?<=<client>)[^<]+" config.xml)
+ROOT_DIR=$(grep -oPm1 "(?<=<root_path>)[^<]+" config.xml)
 
 # Verwijder irritante cache folder (kan error geven als directory niet bestaat)
-rm -rf /root/Automation3/SERVER_CLIENT/__pycache__
+rm -rf $ROOT_DIR/SERVER_CLIENT/__pycache__
 
 # Run dependencies.sh en format de output
 echo "Runing dependencies.sh to check for installed software ..."
-bash /root/Automation3/INSTALL/dependencies.sh | column -t
+bash $ROOT_DIR/INSTALL/dependencies.sh | column -t
 
 # Als niet alle dependencies geïnstalleerd zijn moet het script stoppen ...
-if  /root/Automation3/INSTALL/dependencies.sh | grep '[X]'; then
+if  $ROOT_DIR/INSTALL/dependencies.sh | grep '[X]'; then
 	echo "Error, not all dependencies are installed. Exiting script."
 	exit
 # ... en anders mag het doorgaan
@@ -20,7 +22,7 @@ fi
 
 ## Run het Python script dat een script signed.
 echo "Sign a script"
-/usr/bin/python3 /root/Automation3/SIGNING/create_sum.py 
+/usr/bin/python3 $ROOT_DIR/SIGNING/create_sum.py 
 
 # Run ansible's 'ping' module om te kijken of de client online is
 echo "Testing whether ansible client is online ..."
@@ -35,12 +37,12 @@ fi
 
 # Voer de ansible playbooks uit
 echo "Installing scripts on client ..."
-ansible-playbook /root/Automation3/SERVER_CLIENT/scripts.yml && ansible-playbook /root/Automation3/SERVER_CLIENT/sysd.yml
+ansible-playbook $ROOT_DIR/SERVER_CLIENT/scripts.yml && ansible-playbook /root/Automation3/SERVER_CLIENT/sysd.yml
 echo "SCRIPTS installed sucessfully"
 
 # Check SHA1 sum op client (over ssh)
 echo "Checking SHA1 sum of scripts on client ..."
-if ssh 192.168.213.143 python3 /root/Automation3/SIGNING/check_sum.py | grep 'failed'; then
+if ssh $CLIENT_IP python3 $ROOT_DIR/SIGNING/check_sum.py | grep 'failed'; then
 	# Exit als SHA1 sum niet klopt
 	echo "SHA1 sums are not correct. Exiting script."
 	exit
@@ -50,7 +52,7 @@ echo "SHA1 sums are correct."
 # SIGN SCRIPTS
 
 # Installeer de listen systemd service
-cp /root/Automation3/SERVER_CLIENT/monitor-listen.service /etc/systemd/system/ && systemctl daemon-reload && systemctl --now enable monitor-listen.service
+cp $ROOT_DIR/SERVER_CLIENT/monitor-listen.service /etc/systemd/system/ && systemctl daemon-reload && systemctl --now enable monitor-listen.service
 if systemctl status monitor-listen | grep -i "active (running)"; then
 	echo "Systemd service is running."
 else
@@ -64,12 +66,12 @@ if systemctl status docker | grep -i "active (running)"; then
 	echo "Docker is running, rolling out container ..."
 else
 	echo "Docker is not running. Exiting script."
-	#exit
+	exit
 fi
 
 # Change directory naar SCRIPTS/ en start de website met docker-compose
-cd /root/Automation3/httpd/Docker && docker-compose up &
+cd $ROOT_DIR/httpd/Docker && docker-compose up &
 echo "System installed successfully. Load monitoring.local in a browser to monitor"
 
 # Run Selenium
-/usr/bin/python3 /root/Automation3/TESTING/test.py
+/usr/bin/python3 $ROOT_DIR/TESTING/test.py
