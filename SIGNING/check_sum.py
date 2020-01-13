d30b8e1322b0c1891097638310bcdbaaafdfe772
bart <bart.hakstege@student.hu.nl>
2020-01-13 10:26:29 +0100

    Updated checksum scripts

--- SIGNING/check_sum.py
+++ SIGNING/check_sum.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 def check_sum():
-	cmd = "sha1sum -c /root/Scripts/.sha1"
+	cmd = "sha1sum -c /root/Automation3/SERVER_CLIENT/.sha1"
 	return_value = subprocess.call(cmd, shell=True) # returned de exit code
 	if return_value == 0:
 		print("SHA1 sum is correct")
