9fedaa8c5c271b3e576a55ece6972a3e43cd1699
bart <bart.hakstege@student.hu.nl>
2020-01-13 13:11:28 +0100

    Replaced 'echo' with 'printf' to allow new-lines

--- INSTALL/install.sh
+++ INSTALL/install.sh
@@ -10,70 +10,69 @@ ROOT_DIR=$(grep -oPm1 "(?<=<root_path>)[^<]+" /root/Automation3/INSTALL/config.x
 rm -rf $ROOT_DIR/SERVER_CLIENT/__pycache__
 
 # Run dependencies.sh en format de output
-echo "Runing dependencies.sh to check for installed software ..."
+printf "\nRuning dependencies.sh to check for installed software ..."
 bash $ROOT_DIR/INSTALL/dependencies.sh | column -t
 
 # Als niet alle dependencies geïnstalleerd zijn moet het script stoppen ...
 if  $ROOT_DIR/INSTALL/dependencies.sh | grep '[X]'; then
-	echo "Error, not all dependencies are installed. Exiting script."
+	printf "Error, not all dependencies are installed. Exiting script."
 	exit
 # ... en anders mag het doorgaan
 else
-	echo "All dependencies are installed, continuing ..."
+	printf "\nAll dependencies are installed, continuing ..."
 fi
 
 ## Run het Python script dat een script signed.
-echo "Sign a script"
 /usr/bin/python3 $ROOT_DIR/SIGNING/create_sum.py 
 
 # Run ansible's 'ping' module om te kijken of de client online is
-echo "Testing whether ansible client is online ..."
+printf "\Testing whether ansible client is online ..."
 
 if ansible -m ping all | grep 'pong'; then
-	echo "Client is online"
+	printf "\nClient is online"
 else
 	# Exit script als client niet online is.
-	echo "Client is not online. Exiting script."
+	printf "\nClient is not online. Exiting script."
 	exit
 fi
 
 # Voer de ansible playbooks uit
-echo "Installing scripts on client ..."
+printf "\nInstalling scripts on client ..."
 ansible-playbook $ROOT_DIR/SERVER_CLIENT/scripts.yml && ansible-playbook /root/Automation3/SERVER_CLIENT/sysd.yml
-echo "SCRIPTS installed sucessfully"
+printf "\nSCRIPTS installed sucessfully"
 
 # Check SHA1 sum op client (over ssh)
-echo "Checking SHA1 sum of scripts on client ..."
+printf "\nChecking SHA1 sum of scripts on client ..."
 if ssh $CLIENT_IP python3 $ROOT_DIR/SIGNING/check_sum.py | grep 'failed'; then
 	# Exit als SHA1 sum niet klopt
-	echo "SHA1 sums are not correct. Exiting script."
+	printf "SHA1 sums are not correct. Exiting script."
 	exit
 fi
-echo "SHA1 sums are correct."
+printf "\nSHA1 sums are correct."
 
 # SIGN SCRIPTS
 
 # Installeer de listen systemd service
 cp $ROOT_DIR/SERVER_CLIENT/monitor-listen.service /etc/systemd/system/ && systemctl daemon-reload && systemctl --now enable monitor-listen.service
 if systemctl status monitor-listen | grep -i "active (running)"; then
-	echo "Systemd service is running."
+	printf "\nSystemd service is running."
 else
 	# Exit als de service niet runt
-	echo "Systemd service is not running. Exiting script."
+	printf "\nSystemd service is not running. Exiting script."
 	exit
 fi
 
 # Controleer of docker active is
 if systemctl status docker | grep -i "active (running)"; then
-	echo "Docker is running, rolling out container ..."
+	printf "\nDocker is running, rolling out container ..."
 else
-	echo "Docker is not running. Exiting script."
+	printf "\nDocker is not running. Exiting script."
 	exit
 fi
 
 # Change directory naar SCRIPTS/ en start de website met docker-compose
 cd $ROOT_DIR/httpd/Docker && docker-compose up &
-echo "System installed successfully. Load monitoring.local in a browser to monitor"
+printf "\nSystem installed successfully. Load monitoring.local in a browser to monitor"
 
 # Run Selenium
 /usr/bin/python3 $ROOT_DIR/TESTING/test.py
